ce47fca002c548e954989321bef9f64b78621b83
Алексей Костин <php.programmist.ru@gmail.com>
2021-02-06 14:27:18 +0300

    updateId field added to Message

---
+++ migrations/Version20210206112612.php
@@ -0,0 +1,33 @@
+<?php
+
+declare(strict_types=1);
+
+namespace DoctrineMigrations;
+
+use Doctrine\DBAL\Schema\Schema;
+use Doctrine\Migrations\AbstractMigration;
+
+/**
+ * Auto-generated Migration: Please modify to your needs!
+ */
+final class Version20210206112612 extends AbstractMigration
+{
+    public function getDescription() : string
+    {
+        return '';
+    }
+
+    public function up(Schema $schema) : void
+    {
+        // this up() migration is auto-generated, please modify it to your needs
+        $this->addSql('ALTER TABLE message ADD update_id INT NOT NULL, CHANGE telegram_date telegram_date DATETIME DEFAULT NULL');
+        $this->addSql('CREATE UNIQUE INDEX unique_update_id ON message (update_id)');
+    }
+
+    public function down(Schema $schema) : void
+    {
+        // this down() migration is auto-generated, please modify it to your needs
+        $this->addSql('DROP INDEX unique_update_id ON message');
+        $this->addSql('ALTER TABLE message DROP update_id, CHANGE telegram_date telegram_date DATETIME NOT NULL');
+    }
+}
--- src/Entity/Message.php
+++ src/Entity/Message.php
@@ -3,10 +3,14 @@
 namespace App\Entity;
 
 use App\Repository\MessageRepository;
+use DateTimeInterface;
 use Doctrine\ORM\Mapping as ORM;
 
 /**
  * @ORM\Entity(repositoryClass=MessageRepository::class)
+ * @ORM\Table(uniqueConstraints={
+ *     @ORM\UniqueConstraint(name="unique_update_id",columns={"update_id"})
+ * })
  */
 class Message
 {
@@ -35,9 +39,15 @@ class Message
     private $createdAt;
 
     /**
-     * @ORM\Column(type="datetime")
+     * @ORM\Column(type="datetime",nullable=true)
      */
     private $telegramDate;
+
+    /**
+     * @ORM\Column(type="integer")
+     * @var int
+     */
+    private $updateId;
 
     public function getId(): ?int
     {
@@ -68,27 +78,45 @@ class Message
         return $this;
     }
 
-    public function getCreatedAt(): ?\DateTimeInterface
+    public function getCreatedAt(): ?DateTimeInterface
     {
         return $this->createdAt;
     }
 
-    public function setCreatedAt(\DateTimeInterface $createdAt): self
+    public function setCreatedAt(DateTimeInterface $createdAt): self
     {
         $this->createdAt = $createdAt;
 
         return $this;
     }
 
-    public function getTelegramDate(): ?\DateTimeInterface
+    public function getTelegramDate(): ?DateTimeInterface
     {
         return $this->telegramDate;
     }
 
-    public function setTelegramDate(\DateTimeInterface $telegramDate): self
+    public function setTelegramDate(?DateTimeInterface $telegramDate): self
     {
         $this->telegramDate = $telegramDate;
 
+        return $this;
+    }
+
+    /**
+     * @return int
+     */
+    public function getUpdateId(): int
+    {
+        return $this->updateId;
+    }
+
+    /**
+     * @param int $updateId
+     * @return $this
+     */
+    public function setUpdateId(int $updateId): self
+    {
+        $this->updateId = $updateId;
         return $this;
     }
 }
